ed6c6c5bdb5a1ccab1670f555db385455cd87bbe
hamadastore22 <hamadastore22@gmail.com>
2024-06-24 08:11:29 -0700

    E6-basic is updated

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -2,12 +2,12 @@ export default function taskBlock(trueOrFalse) {
   const task = false;
   const task2 = true;
 
-	/* eslint-disable no-unsed-vars */
+  /* eslint-disable no-unused-vars */
   if (trueOrFalse) {
     const task = true;
     const task2 = false;
   }
+  /* eslint-enable no-unused-vars */
 
-	/* eslint-enable no-unsed-vars */
   return [task, task2];
 }
